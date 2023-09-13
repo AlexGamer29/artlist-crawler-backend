c2b6cf2bb19773de434a4c68198f7dd427d29f71
Alexander Minh-Duc Nguyen <sun123vet@gmail.com>
2023-09-13 03:37:33 +0000

    add: args for puppeteer.launch

--- src/app/controller/artlist.js
+++ src/app/controller/artlist.js
@@ -5,7 +5,16 @@ async function init(link) {
   // Launch a headless browser
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox'],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
   });
 
   const mediaFiles = [];
